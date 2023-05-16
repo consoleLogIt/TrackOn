5481cb14a90fffb5bb0d044eeb1743f737307f89
consoleLogIt <kishansingh4do@gmail.com>
2023-05-16 10:58:29 +0530

    display correct time

--- src/components/calendar/components/eventBlock/index.jsx
+++ src/components/calendar/components/eventBlock/index.jsx
@@ -4,13 +4,14 @@ import {
   EventTimeStyled,
   EventTitleStyled,
 } from "./styled";
+import { getTimeDisplayNew } from "../CalendarDay";
 
 export default function EventBlock({ title, timeRange, style, color }) {
   return (
     <EventContainerStyled {...style} color={color}>
       <EventTitleStyled>{title}</EventTitleStyled>
       <EventTimeStyled>
-        {`${timeRange[0]} -- ${timeRange[1]}`}
+        {`${getTimeDisplayNew(timeRange[0])} -- ${getTimeDisplayNew(timeRange[1])}`}
       </EventTimeStyled>
     </EventContainerStyled>
   );
